5de8013736bf1c7477d48d6a0d62fecf5e7ebc7a
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-09 12:33:38 +0500

    📦 NEW: s3 implemented

--- routes/editnode.js
+++ routes/editnode.js
@@ -4,65 +4,99 @@ const getnodes = require("../models/sankeynodes");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const AWS = require("aws-sdk"); // Add AWS SDK
+
+ 
 
 // Configure Multer for storing profile images
-const storage = multer.diskStorage({
-    // Setup destination folder for uploaded images
-    destination: function (req, file, next) {
-        next(null, "public/nodes");
-    },
-    // Setup filename for uploaded image
-    filename: function (req, file, next) {
-        let { old_id, new_id } = req.body;
-
-        // Get the file extension from the original file name
-        let fileExtension = file.originalname.split(".").pop();
-
-        // Generate a unique file name using new_id or old_id, current timestamp, and file extension
-        let fileName = new_id ? new_id + Date.now() + "." + fileExtension : old_id + Date.now() + "." + fileExtension;
-
-        next(null, fileName);
-    },
-});
+const storage = multer.memoryStorage(); // Use memory storage for uploading to S3
 
 const nodes = multer({ storage });
+// Configure AWS SDK
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+ 
+
+const s3 = new AWS.S3();
+
+ 
 
 router.put("/", nodes.single("image"), async (req, res) => {
-   try {
+  try {
     const { role } = req.authData;
-    if(req.authData)
-    if (role !== "admin") {
-        throw { status: 403, message: "Access forbidden. You are not an admin." };
+    if (req.authData)
+      if (role !== "admin") {
+        throw {
+          status: 403,
+          message: "Access forbidden. You are not an admin.",
+        };
       }
-    const { old_id, new_id , colour } = req.body;
 
-   if(!old_id ) return res.status(400).send("Required field cannot be empty");
-   
-    const node = await getnodes.findOne({ where:{id:old_id}})
-    if (!node) return res.status(400).send('No such node exists.');
+ 
 
-    const imagepath = path.join(__dirname, '..', 'public/nodes', node.image)
-    if (fs.existsSync(imagepath) && node.image && req?.file?.filename) fs.unlinkSync(imagepath) 
+    const { old_id, new_id, colour } = req.body;
 
-    const img = req.file ? req.file.filename: node.image;
+ 
 
+    if (!old_id) return res.status(400).send("Required field cannot be empty");
+
+ 
+
+    const node = await getnodes.findOne({ where: { id: old_id } });
+    if (!node) return res.status(400).send("No such node exists.");
+
+ 
+
+    const imagepath = path.join(__dirname, "..", "public/nodes", node.image);
+    if (
+      fs.existsSync(imagepath) &&
+      node.image &&
+      req?.file?.buffer
+    )
+      fs.unlinkSync(imagepath);
+
+ 
+
+    let img = node.image;
+    // Upload to S3 if a new image is provided
+    if (req.file) {
+      const fileExtension = req.file.originalname.split(".").pop();
+      const fileName = `${new_id ? new_id : old_id}-${Date.now()}.${fileExtension}`;
+      const params = {
+        Bucket: "nodes-8-8-23", // Replace with your S3 bucket name
+        Key: fileName,
+        Body: req.file.buffer,
+        ACL: "",
+      };
+      const uploadResult = await s3.upload(params).promise();
+      img = uploadResult.Location;
+    }
     let updatednode = await getnodes.update(
-        {
-            id: new_id,
-            colour: colour ? colour: node.colour,
-            image: img
-        },
-        {
-            where: { id: old_id},
-            returning: true,
-            plain: true,
-        }
+      {
+        id: new_id,
+        colour: colour ? colour : node.colour,
+        image: img,
+      },
+      {
+        where: { id: old_id },
+        returning: true,
+        plain: true,
+      }
     );
-    return res.status(200).send({ message:"Node updated successfully"});
-   } catch (err) {
+
+ 
+
+    return res.status(200).send({ message: "Node updated successfully" });
+  } catch (err) {
     console.log(err);
     res.status(400).send("Something went wrong!");
-   }
+  }
 });
+
+ 
 
 module.exports = router;
